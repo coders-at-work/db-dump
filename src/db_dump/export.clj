f296245b931f8ff7e4ff04dabb4bba700975d3e3
gzeureka <gzeureka@gmail.com>
2017-02-24 23:06:47 +0800

    Comment out pseudo code

--- src/db_dump/export.clj
+++ src/db_dump/export.clj
@@ -16,3 +16,37 @@
 #_(defn export-data [data tagger]
     (tagger data)
     )
+
+; (defn export-table-tree [parent parent-condition [{:table-name :a :fk-name :p-id :ref :id} [b c]]]
+
+; (def rel-definition ^{:root :benefit} {:benefit [{:talbe :benefit-detail :fk :benefit-id}
+;                                {:table :benefit-card-type :fk :benefit-id}]
+;                      :benefit-card-type [{:table :benefit-card-type-detail :fk :benefit-card-type_id}]
+;                      :promotion 
+;                      })
+
+; (def benefit-rel-def
+; {:benefit [{:talbe :benefit-detail :fk :benefit-id} {:table :benefit-card-type :fk :benefit-id}]
+;                      :benefit-card-type [{:table :benefit-card-type-detail :fk :benefit-card-type_id}]
+;                      }
+;   )
+; (def promo-rel-def
+; {:promo [{:talbe :benefit-detail :fk :benefit-id} {:table :benefit-card-type :fk :benefit-id}]
+;                      :benefit-card-type [{:table :benefit-card-type-detail :fk :benefit-card-type_id}]
+;                      }
+
+;   )
+
+; (defmacro action-export [ACTION-TYPE def]
+;   `(defn export-benefit))
+
+; (export-benefit '(= :id 1))
+
+
+; (when has-benefit-action-type
+;   (export-table-tree :benefit '(= :id 1) benefit-rel-definition)
+; )
+
+; (when has-promo-action-type
+; (export-table-tree '(= :id 1) promo-rel-definition)
+; )
