047de7a3f62a4d5d7c2a85d4d3f8355f48f7448d
gzeureka <gzeureka@gmail.com>
2017-02-28 19:56:07 +0800

    clean up code

--- src/db_dump/import.clj
+++ src/db_dump/import.clj
@@ -6,46 +6,28 @@
             [ladybird.data.cond :refer (make)]
             [ladybird.db.core :as db]
             [ladybird.db.transaction :refer (do-tx)]
-            [korma.core :refer (exec-raw)]
             [korma.db :as kdb]
             [clojure.java.jdbc :as jdbc]
             )
   )
 
 ; TODO extract set identity insert on/off function, how about database other then mssql?
-; TODO refacto exec-raw to ladybird?
 (defn enable-insert-into-identity [db-conn table-name]
-  ; (exec-raw db-conn (format "SET IDENTITY_INSERT %s ON" (name table-name)))
-  ; (let [conn kdb/*current-conn*
-  ;       st (.createStatement conn)]
-    ; (println :on-conn conn)
-    ; (.execute st (format "SET IDENTITY_INSERT %s ON" (name table-name)))
-
-    ; )
-    (jdbc/db-do-commands db-conn (format "SET IDENTITY_INSERT %s ON" (name table-name)))
-  (println :ON)
+  (jdbc/db-do-commands db-conn (format "SET IDENTITY_INSERT %s ON" (name table-name)))
   )
 
 ; TODO extract set identity insert on/off function, how about database other then mssql?
 (defn disable-insert-into-identity [db-conn table-name]
-  ; (exec-raw db-conn (format "SET IDENTITY_INSERT %s OFF" (name table-name)))
-  ; (let [conn kdb/*current-conn*
-  ;       st (.createStatement conn)]
-  ;   (println :off-conn conn)
-  ;   (.execute st (format "SET IDENTITY_INSERT %s OFF" (name table-name)))
-  ;   )
-
-    (jdbc/db-do-commands db-conn (format "SET IDENTITY_INSERT %s ON" (name table-name)))
-  (println :OFF)
+  (jdbc/db-do-commands db-conn (format "SET IDENTITY_INSERT %s ON" (name table-name)))
   )
 
-; TODO how to detect version row
+; TODO how do we know which column is rowversion data type
 (defn insert-row [table-name {:keys [id] :as row}]
   (log/debug "inserting record of id" id)
   (add! table-name (dissoc row :version))
   )
 
-; TODO how to detect version row
+; TODO how do we know which column is rowversion data type
 (defn update-row [table-name {:keys [id] :as row}]
   (log/debug "updating record of id" id)
   (modify! table-name (make (= :id id)) (dissoc row :id :version))
@@ -55,7 +37,6 @@
   (let [{:keys [dict rows]:as data} (read-from (file dir file-name))
         {:keys [table-name]} dict
         row-count (count rows)]
-    ; TODO write data to db
     (log/debug (format "importing %d records into table %s" row-count table-name))
     (when (pos? row-count)
       (let [db-conn (db/get-cur-db-conn)]
@@ -67,7 +48,6 @@
                    (insert-row table-name row)
                    )
                  )
-               ; (exec-raw (db/get-cur-db-conn) "insert into cms_control (id, user_id, name, value ,create_time, last_update) values (10, 1, 'E', 'E', current_timestamp, current_timestamp);" :results)
                (disable-insert-into-identity kdb/*current-conn* table-name)
                )
         )
