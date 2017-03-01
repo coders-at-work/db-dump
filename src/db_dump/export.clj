e3c1bb6aa50ae8851479e6a5201d17d0e194a622
xzj <xzj007@gmail.com>
2017-03-01 22:19:21 +0800

    Exports a table tree to file.
    	modified:   src/db_dump/export.clj

--- src/db_dump/export.clj
+++ src/db_dump/export.clj
@@ -20,12 +20,65 @@
   ([dir table-name]
    (export-table-to-file dir table-name nil))
   ([dir table-name condition]
-   (write-to (file dir (name table-name)) (export-table table-name condition))
-   )
-  )
+   (let [data (export-table table-name condition)]
+     (write-to (file dir (name table-name)) data)
+     data)))
+
+(defn make-child-condition [parent-recs {:keys [fk ref] :as child-def}]
+  (->> parent-recs (map #(ref %)) (list 'in fk)))
+
+(defn export-child
+  "
+   child-def should be:
+     {:table  :benefit_detail
+      :parent :benefit
+      :fk     :benefit_id
+      :ref    :id
+      }
+  "
+  [export-fn parent-recs {:keys [table] :as child-def}]
+  (let [condition (make-child-condition parent-recs child-def)]
+    (export-fn table condition)))
+
+(defn export-child-to-file
+  [dir parent-recs child-def]
+  (export-child (partial export-table-to-file dir) parent-recs child-def))
+
+(defn export-parent
+  [export-fn table condition]
+  (export-fn table condition))
+
+(defn export-parent-to-file
+  [dir table condition]
+  (export-parent (partial export-table-to-file dir) table condition))
+
 
 (defn export-table-tree-to-file
-  [dir root-table condition table-rel-def])
+  "
+   table-rel-def should be:
+     [
+      {:table :benefit
+       :parent nil
+      }
+      {:table :benefit_detail
+       :parent :benefit
+       :fk :benefit_id
+      }
+     ]
+    Parent table must be prior to child table.
+  "
+  [dir [parent-table-def & children-table-def :as table-rel-def] condition]
+  (let [{:keys [table]} parent-table-def
+        data (export-parent-to-file dir table condition)
+        ]
+    (loop [parent-data-m {table (:rows data)}
+           children-table-def children-table-def
+           ]
+          (when-let [{:keys [parent] :as table-def} (first children-table-def)]
+                    (let [m (->> (export-child-to-file dir (parent-data-m table) table-def)
+                                 :rows
+                                 (assoc parent-data-m parent))]
+                      (recur m  (rest children-table-def)))))))
 
 #_(defn export-data [data tagger]
     (tagger data)
