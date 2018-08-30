6bfcefe462515d05151e32a1e83c82a7f1fcd29a
gzeureka <gzeureka@gmail.com>
2018-08-30 17:26:14 +0800

    upgrade project version to 0.2.0-SNAPSHOT, upgrade timbre from 4.1.4 to 4.10.0.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject coders-at-work/db-dump "0.1.0-SNAPSHOT"
+(defproject coders-at-work/db-dump "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.1.4"]
+                 [com.taoensso/timbre "4.10.0"]
                  [clj-time "0.11.0"]
                  [me.raynes/fs "1.4.6"]
                  [coders-at-work/ladybird "0.5.3-SNAPSHOT"]
